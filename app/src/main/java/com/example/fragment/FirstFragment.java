a95eae286e81ff8bc1673b1de07a5dabd1886ee9
trucka007 <56639995+trucka007@users.noreply.github.com>
2019-10-16 20:26:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fragment/FirstFragment.java
@@ -0,0 +1,45 @@
+package com.example.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+
+    Button button;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_first, container, false);
+
+        button=view.findViewById(R.id.bt_f);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MainActivity.fragmentManager.beginTransaction().replace(R.id.fragment_container, new SecondFragment(), null).addToBackStack(null).commit();
+
+            }
+        });
+
+        return view;
+    }
+
+}
